756ca7af7526bb9610cef8eb453458712633cefd
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-03-09 18:19:48 +0200

    Refactor webpack config file for React build

---
+++ RealTimeChatApp/webpack.config.js
@@ -0,0 +1,39 @@
+﻿const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+// Configures the Webpack compilation process:
+// The output property overrides the default value of dist.
+// The bundle is instead emitted in the wwwroot directory.
+// The resolve.extensions array includes.js to imp.
+
+module.exports = {
+    entry: path.resolve(__dirname, "../client/chat-app/build/index.html"),
+    output: {
+        path: path.resolve(__dirname, "wwwroot"),
+        filename: "[name].[chunkhash].js",
+        publicPath: "/",
+    },
+    resolve: {
+        extensions: [".js", ".ts", ".tsx"],
+    },
+    module: {
+        rules: [
+            {
+                test: /\.ts$/,
+                use: "ts-loader",
+            },
+            {
+                test: /\.css$/,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+        ],
+    },
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin({
+            filename: "css/[name].[chunkhash].css",
+        }),
+    ],
+};
