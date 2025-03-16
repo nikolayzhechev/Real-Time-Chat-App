d678fe031bbd4287f69432f7f0c9bc4ccb415dd5
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-03-16 17:15:21 +0200

    Improve CORS service code.

--- RealTimeChatApp/Program.cs
+++ RealTimeChatApp/Program.cs
@@ -4,10 +4,42 @@ internal class Program
     private static void Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
+
+        // Add controllers to the service container
+        builder.Services.AddControllers();
+
+        /*
+        // Add CORS services
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowAll",
+                policy =>
+                {
+                    policy.AllowAnyOrigin()
+                          .AllowAnyMethod()
+                          .AllowAnyHeader();
+                });
+        });
+        */
+        // Allow CORS for React
+        builder.Services.AddCors(options =>
+        {
+            options.AddPolicy("AllowReact",
+                policy =>
+                {
+                    policy.WithOrigins("http://localhost:3000", "https://localhost:7250") // React Dev Server
+                          .AllowAnyMethod()
+                          .AllowAnyHeader()
+                          .AllowCredentials();
+                });
+        });
+
         // Add SignalR service
         builder.Services.AddSignalR();
 
         var app = builder.Build();
+
+        app.UseCors("AllowReact");  // Ensure CORS middleware is used
 
         // HTTP request pipeline
         if (!app.Environment.IsDevelopment())
@@ -16,17 +48,14 @@ internal class Program
             app.UseHsts();
         }
 
-        // Enable CORS
-        app.UseCors(builder =>
-            builder.AllowAnyOrigin()
-            .AllowAnyMethod()
-            .AllowAnyHeader()
-            .WithOrigins("http://localhost:3000/")
-            .AllowCredentials()
-        );
-
+        // Redirect to index.html if no route is specified
         app.UseDefaultFiles();
+        // Serve static files from wwwroot
         app.UseStaticFiles();
+
+        app.UseRouting();
+        app.UseAuthorization();
+        app.MapControllers();
 
         app.MapHub<ChatHub>("/hub");
 
