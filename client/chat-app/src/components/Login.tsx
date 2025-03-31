d714746123e11f9f55212649c641c3485b17af5d
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-03-31 20:35:11 +0300

    Initial client login capabilities
    Add initial login capabilities to client side.

--- client/chat-app/src/components/Login.tsx
+++ client/chat-app/src/components/Login.tsx
@@ -1,7 +1,27 @@
 import React, { ReactElement, useState, ChangeEvent, FormEvent } from 'react';
 
 function Login({onLoginData}: any): ReactElement {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
   const [inputValue, setInputValue] = useState('');
+
+    const handleLogin = async (event: FormEvent<HTMLFormElement>): Promise<void> => {
+      handleSubmit(event);
+
+      const response = await fetch('http://localhost:5258/api/auth/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({email, password})
+      });
+
+      if (response.ok) {
+        const data = await response.json();
+        localStorage.setItem('token', data.token);
+        console.log('Login successful!');
+      } else {
+        console.log('Login failed!');
+      }
+    };
 
     const handleChange = (event: ChangeEvent<HTMLInputElement>): void => {
       setInputValue(event.target.value);
@@ -16,8 +36,8 @@ function Login({onLoginData}: any): ReactElement {
     return (
         <div>
             <h2>Username Form</h2>
-            <form onSubmit={handleSubmit}>
-              <label htmlFor="username">Enter your username:</label><br /><br />
+            <form onSubmit={handleLogin}>
+              <label htmlFor="username">Enter your username:</label>
               <input 
                 type="text" 
                 id="username" 
@@ -25,7 +45,9 @@ function Login({onLoginData}: any): ReactElement {
                 value={inputValue} 
                 onChange={handleChange} 
                 required 
-              /><br /><br />
+              />
+              <input type="email" value={email} onChange={e => setEmail(e.target.value)} placeholder="Email" />
+              <input type="password" value={password} onChange={e => setPassword(e.target.value)} placeholder="Password" />
               <input type="submit" value="Submit" />
             </form>
         </div>
