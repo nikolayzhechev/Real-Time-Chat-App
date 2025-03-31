8939dca78f4214eaf42cd7ded2256bbc7391f091
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-03-31 20:37:17 +0300

    Create auth controller class
    Handles authentication

---
+++ RealTimeChatApp/Authentication/AuthController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace RealTimeChatApp.Authentication
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AuthController : ControllerBase
+    {
+        [HttpPost("login")]
+        public IActionResult Login([FromBody] LoginRequest request)
+        {
+            // Hardcode TEST credentions
+            // TODO: update with database user check
+            if (request.Email == "test@example.com" & request.Password == "password1234")
+            {
+                var tokenHandler = new JwtSecurityTokenHandler();
+                // Hardcode secret key for testing
+                // TODO: update secret key in appsettings.json or env variable
+                var key = Encoding.ASCII.GetBytes("secret_key_01");
+                var tokenDescriptor = new SecurityTokenDescriptor
+                {
+                    Subject = new ClaimsIdentity(new[]
+                    {
+                        new Claim(ClaimTypes.Name, request.Email)
+                    }),
+                    Expires = DateTime.UtcNow.AddHours(1),
+                    SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+                };
+                var token = tokenHandler.CreateToken(tokenDescriptor);
+                var tokenString = tokenHandler.WriteToken(token);
+
+                return Ok(new { token = tokenString });
+            }
+
+            return Unauthorized();
+        }
+    }
+}
