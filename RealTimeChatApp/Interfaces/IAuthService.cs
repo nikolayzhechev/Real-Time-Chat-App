5027cecb7d0316dd597429b48ab06636fe5e9323
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-04-13 22:18:56 +0300

    Commit auth service interface

---
+++ RealTimeChatApp/Interfaces/IAuthService.cs
@@ -0,0 +1,11 @@
+﻿using RealTimeChatApp.Authentication;
+
+namespace RealTimeChatApp.Interfaces
+{
+    public interface IAuthService
+    {
+        public bool Register(RegisterRequest request);
+        public string Login(LoginRequest request);
+        public User ValidateUser(string email, string password);
+    }
+}
