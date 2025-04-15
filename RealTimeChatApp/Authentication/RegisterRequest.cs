05aa061e3f210d1c2a265330bddc744679b622f1
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-04-15 21:22:13 +0300

    Add registration class

---
+++ RealTimeChatApp/Authentication/RegisterRequest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity.Data;
+using RealTimeChatApp.Interfaces;
+
+namespace RealTimeChatApp.Authentication
+{
+    public class RegisterRequest : IRegisterRequest
+    {
+        public string Email {  get; set; }
+
+        public string UserName {  get; set; }
+
+        public string Password {  get; set; }
+    }
+}
---
+++ RealTimeChatApp/Interfaces/IRegisterRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace RealTimeChatApp.Interfaces
+{
+    public interface IRegisterRequest
+    {
+        public string Email { get; }
+        public string UserName { get; }
+        public string Password { get; }
+    }
+}
