4cbda8ae18854c8693ac4da7e618b4136bd4ba36
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-04-19 16:33:54 +0300

    Create initial database context

---
+++ RealTimeChatApp/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace RealTimeChatApp.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> oprtions) : base(oprtions)
+        {
+        }
+    }
+}
