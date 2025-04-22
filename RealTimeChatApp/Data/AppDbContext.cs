969df08b64cf36ebf81126ec2e28bed27a92844e
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-04-22 21:44:28 +0300

    Set properties for each db table
    Add properties in the db context

--- RealTimeChatApp/Data/AppDbContext.cs
+++ RealTimeChatApp/Data/AppDbContext.cs
@@ -1,11 +1,17 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using RealTimeChatApp.Models;
 
 namespace RealTimeChatApp.Data
 {
     public class AppDbContext : DbContext
     {
-        public AppDbContext(DbContextOptions<AppDbContext> oprtions) : base(oprtions)
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
         {
         }
+
+        public DbSet<AppUser> AppUsers { get; set; }
+        public DbSet<Chat> Chats { get; set; }
+        public DbSet<ChatUser> ChatUsers { get; set; }
+        public DbSet<Message> Messages { get; set; }
     }
 }
