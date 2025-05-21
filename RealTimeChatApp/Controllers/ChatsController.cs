fd8bc6fadcd6233a2df9823e7170ffda14ee858d
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-05-21 20:03:56 +0300

    Setup chats controller
    Main chat controller

---
+++ RealTimeChatApp/Controllers/ChatsController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using RealTimeChatApp.Data;
+using RealTimeChatApp.Models;
+
+namespace RealTimeChatApp.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ChatsController : ControllerBase
+    {
+        private readonly AppDbContext _dBcontext;
+
+        public ChatsController(AppDbContext dBcontext)
+        {
+            _dBcontext = dBcontext;
+        }
+
+        // GET: api/Chats
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Chat>>> GetChats()
+        {
+            return await _dBcontext.Chats.ToListAsync();
+        }
+
+        // GET: api/Chats/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Chat>> GetChat(int id)
+        {
+            var chat = await _dBcontext.Chats.FindAsync(id);
+
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            return chat;
+        }
+
+        // PUT: api/Chats/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutChat(int id, Chat chat)
+        {
+            if (id != chat.Id)
+            {
+                return BadRequest();
+            }
+
+            _dBcontext.Entry(chat).State = EntityState.Modified;
+
+            try
+            {
+                await _dBcontext.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ChatExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Chats
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost("chat")]
+        public async Task<ActionResult<Chat>> PostChat(Chat chat)
+        {
+            _dBcontext.Chats.Add(chat);
+            await _dBcontext.SaveChangesAsync();
+
+            return CreatedAtAction("GetChat", new { id = chat.Id }, chat);
+        }
+
+        // DELETE: api/Chats/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteChat(int id)
+        {
+            var chat = await _dBcontext.Chats.FindAsync(id);
+            if (chat == null)
+            {
+                return NotFound();
+            }
+
+            _dBcontext.Chats.Remove(chat);
+            await _dBcontext.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool ChatExists(int id)
+        {
+            return _dBcontext.Chats.Any(e => e.Id == id);
+        }
+    }
+}
