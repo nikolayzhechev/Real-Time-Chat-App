26d6160b6a297bb43dcb95b536438311e730b900
Nikolay Zhechev <nikolayzhechev@yahoo.com>
2025-09-29 07:55:06 +0300

    Add SignalR and general config

--- client/chat-app/src/SignalR/signalRConnection.ts
+++ client/chat-app/src/SignalR/signalRConnection.ts
@@ -1,14 +1,26 @@
 import * as signalR from "@microsoft/signalr";
 
+let connection: signalR.HubConnection | null = null;
+
 // Set up SignalR to manage the connection
-export const connection = new signalR.HubConnectionBuilder()
-    .withUrl("http://localhost:5258/hub")   // .NET mapping
-    .withAutomaticReconnect([0, 2000, 5000, 10000])
-    .configureLogging(signalR.LogLevel.Information)
-    .build();
+function createConnection(token: string):signalR.HubConnection {
+     return new signalR.HubConnectionBuilder()
+        .withUrl("http://localhost:5258/hub", {
+            accessTokenFactory: () => {
+                return localStorage.getItem("token") || "";
+            }
+        })   // .NET mapping
+        .withAutomaticReconnect([0, 2000, 5000, 10000])
+        .configureLogging(signalR.LogLevel.Information)
+        .build();
+}
 
 // Establise connection before sending messages
-export async function ensureConnected(): Promise<void> {
+export async function ensureConnected(token: string): Promise<void> {
+    if (!connection || connection.state === signalR.HubConnectionState.Disconnected) {
+        connection = createConnection(token);
+    }
+
     console.log("Checking SignalR State:", connection.state);
 
     // Wait for full disconnection before attempting to start
@@ -28,5 +40,24 @@ export async function ensureConnected(): Promise<void> {
         }
     } else {
         console.warn("SignalR is already connecting or connected.");
+    }
+};
+
+// Export connection
+export function getConnection(): signalR.HubConnection | null {
+    return connection;
+}
+
+// On logout
+export async function stopConnection(): Promise<void> {
+    if (connection) {
+        try {
+            await connection.stop();
+            console.log("SignalR connection stopped.");
+        } catch (err) {
+            console.error("Error stopping SignalR connection:", err);
+        } finally {
+            connection = null;
+        }
     }
 };
--- client/chat-app/src/index.tsx
+++ client/chat-app/src/index.tsx
@@ -3,13 +3,19 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { AuthProvider } from './contexts/authContext';
+import { UserProvider } from './contexts/userContext';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <AuthProvider>
+        <UserProvider>
+          <App />
+        </UserProvider>
+    </AuthProvider>
   </React.StrictMode>
 );
 
